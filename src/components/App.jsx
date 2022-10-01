48fc4d34a16642679e6c8833eb9d4b9c64d8734d
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-10-01 20:12:56 +0300

    add hooks to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { InfinitySpin  } from 'react-loader-spinner'
 import { fetchApiByName } from "services/api/api";
 import { Searchbar } from "./Searchbar/Searchbar";
@@ -6,102 +6,85 @@ import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Box, LoadingBox } from "./Box/Box";
 import { LoadMoreButton } from "./Button/Button";
 import { ModalWindow } from "./Modal/Modal";
-export class App extends React.Component {
-  state = {
-    hitsCount: 0,
-    total: 0,
-    page: 1,
-    value: '',
-    images: [],
-    status: 'idle',
-    loadBtnStatus: 'idle',
-    modalStatus: false,
-    modalImg: '',
-  }
-  async componentDidUpdate(prevProps, prevState) {
-    const {value, page, hitsCount, total} = this.state
-    
-    
-    if (value !== prevState.value) {
+export const App = () => {
+  const [hitsCount, setHitsCount] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [page, setPage] = useState(1)
+  const [value, setValue] = useState('')
+  const [images, setImages] = useState([])
+  const [status, setStatus] = useState('idle')
+  const [loadBtnStatus, setLoadBtnStatus] = useState('idle')
+  const [modalStatus, setModalStatus] = useState(false)
+  const [modalImg, setModalImg] = useState('')
 
-      this.setState({ status: 'loader', loadBtnStatus: 'loader'  })
-      
-      const images = await fetchApiByName(value, 1)
+  useEffect(() => {
+    if (value === '') {
+      return;
+    }
 
-      if (images.hits.length === 0) {
-        this.setState({ status: 'rejected' });
+    setLoadBtnStatus('loader')
+    setStatus('loader')
+
+    fetchApiByName(value, page).then(r => {
+
+      if (r.hits.length === 0) {
+        setStatus('rejected')
         return
       }
 
-      this.setState({
-        images: images.hits,
-        page: 1, status: 'resolved',
-        loadBtnStatus: 'resolved',
-        total: images.totalHits,
-        hitsCount: images.hits.length
-      })
+      if (r.totalHits > r.hits.length) {
+        setLoadBtnStatus('resolved')
+      }
 
-      if (images.totalHits <= images.hits.length) {
-        this.setState({ loadBtnStatus: 'rejected', })
-        return
-      } 
-    }
+      setImages(prevImages => [...prevImages, ...r.hits]);
+      setStatus('resolved');
+      setTotal(r.totalHits);
+      setHitsCount(prevHitsCount => prevHitsCount + r.hits.length)
 
-
-    if (page !== prevState.page && page !== 1 && hitsCount < total) {
-
-      this.setState({ status: 'loader', loadBtnStatus: 'loader' })
-      
-      const images = await fetchApiByName(value, page)
-
-      this.setState({
-        images: [...this.state.images, ...images.hits],
-        status: 'resolved',
-        loadBtnStatus: 'resolved',
-        hitsCount: hitsCount + images.hits.length
-      })
-
-
-      if (images.totalHits <= images.hits.length + hitsCount) {
-        this.setState({ loadBtnStatus: 'rejected', })
-        return
-      } 
-
-    }
-  }
-  
-  addNewImagesToGallery = () => {
-      this.setState({page: this.state.page + 1})
-    }
-  
-  getInputValue = (value) => {
-    this.setState({value})
-  }
-
-  onImageClick = (largeImg) => {
-    this.setState({ modalStatus: true, modalImg: largeImg })
+    })
     
+  }, [value, page])
+
+  useEffect(() => {
+    if (hitsCount >= total) {
+      setLoadBtnStatus('rejected')
+    }
+  }, [hitsCount, total])
+  
+  
+  const addNewImagesToGallery = () => {
+      setPage(page + 1)
+    }
+  
+  const getInputValue = (value) => {
+    setImages([])
+    setHitsCount(0)
+    setPage(1)
+    setValue(value)
   }
 
-  onClose = () => {
-    this.setState({modalStatus: false})
+  const onImageClick = (largeImg) => {
+    setModalStatus(true);
+    setModalImg(largeImg)
+  }
+
+  const onClose = () => {
+    setModalStatus(false)
   }
   
   
-  render() {
-    const {status, images, loadBtnStatus, modalStatus, modalImg} = this.state
-  return  <Box>
-    <Searchbar onSubmit={this.getInputValue} />
-    <ImageGallery images={images} onImageClick={this.onImageClick } />
+  
+  return <Box>
+    <Searchbar onSubmit={getInputValue} />
+    <ImageGallery images={images} onImageClick={onImageClick } />
     <LoadingBox>
       {status === 'rejected' && <p>Nothing found</p>}
         {status === 'loader' && <InfinitySpin width='200'color="#4fa94d" />}
-      {loadBtnStatus === 'resolved' && <LoadMoreButton onClick={this.addNewImagesToGallery} />}
+      {loadBtnStatus === 'resolved' && <LoadMoreButton onClick={addNewImagesToGallery} />}
       
     </LoadingBox>
-    {modalStatus && <ModalWindow largeImg={modalImg} onClose={this.onClose} />}
+    {modalStatus && <ModalWindow largeImg={modalImg} onClose={onClose} />}
     </Box>
     
     
   };
-};
